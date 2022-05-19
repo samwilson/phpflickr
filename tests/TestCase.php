5dd09e31f1477db5bc993c319840a783fce998d0
Sam Wilson <sam@samwilson.id.au>
2022-05-19 09:52:57 +0800

    Skip tests if unable to authenticate

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,13 @@ namespace Samwilson\PhpFlickr\Tests;
 
 use OAuth\OAuth1\Token\StdOAuth1Token;
 use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+use Samwilson\PhpFlickr\FlickrException;
 use Samwilson\PhpFlickr\PhpFlickr;
 
 abstract class TestCase extends PhpUnitTestCase
 {
-    /** @var PhpFlickr */
-    private $flickr;
+    /** @var PhpFlickr[] */
+    private $flickrs = [];
 
     /**
      * Get an instance of PhpFlickr, configured by the config.php file in the tests directory.
@@ -19,8 +20,9 @@ abstract class TestCase extends PhpUnitTestCase
      */
     public function getFlickr(bool $authenticate = false): PhpFlickr
     {
-        if ($this->flickr instanceof PhpFlickr) {
-            return $this->flickr;
+        $authed = $authenticate ? 'authed' : 'notauthed';
+        if (isset($this->flickrs[$authed])) {
+            return $this->flickrs[$authed];
         }
 
         // Get config values from env vars or the tests/config.php file.
@@ -35,22 +37,34 @@ abstract class TestCase extends PhpUnitTestCase
             // Skip if no key found, so PRs from forks can still be run in CI.
             static::markTestSkipped('No Flickr API key set.');
         }
-        $this->flickr = new PhpFlickr($apiKey, $apiSecret);
+        try {
+            $this->flickrs[$authed] = new PhpFlickr($apiKey, $apiSecret);
+        } catch (FlickrException $ex) {
+            static::markTestSkipped($ex->getMessage());
+        }
 
         // Authenticate?
         if ($authenticate && !empty($accessToken) && !empty($accessTokenSecret)) {
             $token = new StdOAuth1Token();
             $token->setAccessToken($accessToken);
             $token->setAccessTokenSecret($accessTokenSecret);
-            $this->flickr->getOauthTokenStorage()->storeAccessToken('Flickr', $token);
+            $this->flickrs[$authed]->getOauthTokenStorage()->storeAccessToken('Flickr', $token);
+            try {
+                $authenticated = $this->flickrs[$authed]->test()->login();
+            } catch (FlickrException $e) {
+                $authenticated = false;
+            }
+            if (!$authenticated) {
+                static::markTestSkipped('Unable to authenticate with provided access token.');
+            }
         }
         if ($authenticate && empty($accessToken)) {
             static::markTestSkipped(
                 'Access token required for this test. '
-                . 'Please use examples/get_auth_token.php to get one to add to tests/config.php'
+                . 'Please use examples/get_auth_token.php to get token to add to tests/config.php.'
             );
         }
 
-        return $this->flickr;
+        return $this->flickrs[$authed];
     }
 }
