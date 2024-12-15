b96dee920ee17985116b5a8e4fcf8385b1429f5f
Sam Wilson <sam@samwilson.id.au>
2024-12-16 07:39:41 +0800

    Add vendor dir to Phan, fix other errors

--- .phan/config.php
+++ .phan/config.php
@@ -3,6 +3,7 @@
 return [
     'directory_list' => [
         'src',
+        'vendor',
     ],
     'exclude_file_regex' => '@^vendor/.*/(tests?|Tests?)/@',
     'exclude_analysis_directory_list' => [
